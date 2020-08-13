63b7fd40e6ee7b3c24d58555a88a908ac261be23
nickjlupu <nicklupu7@gmail.com>
2020-08-13 00:14:24 -0500

    working on challenge

--- static/js/app.js
+++ static/js/app.js
@@ -30,10 +30,34 @@ var filters = {};
 function updateFilters() {
 
   // Save the element, value, and id of the filter that was changed
-  filters.date = d3.select("#datetime").property("value");
+  filters.datetime = d3.select("#datetime").property("value");
+  filters.city = d3.select("#city").property("value");
+  filters.state = d3.select("#state").property("value");
+  filters.country = d3.select("#country").property("value");
+  filters.shape = d3.select("#shape").property("value");
+  // console.log(filters)
 
   // If a filter value was entered then add that filterId and value
   // to the filters list. Otherwise, clear that filter from the filters object
+  // https://stackoverflow.com/questions/25421233/javascript-removing-undefined-fields-from-an-object
+  Object.keys(filters).forEach(key => filters[key] === "" ? delete filters[key] : {});
+  
+  
+  console.log(filters)
+  // let cleanFiltersArray = []
+  // let filtersArray = Object.entries(filters);
+  // filtersArray.forEach(([key, value]) => {
+  //   cleanFiltersArray.push(value);
+  // });
+  
+  // console.log(cleanFiltersArray);
+
+  // cleanFiltersArray = cleanFiltersArray.filter(item => item);
+  
+  
+  // console.log(cleanFiltersArray);
+
+
 
   // Call function to apply all filters and rebuild the table
   filterTable();
@@ -42,18 +66,29 @@ function updateFilters() {
 function filterTable() {
 
   // Set the filteredData to the tableData
+  let filteredData = tableData;
 
   // Loop through all of the filters and keep any data that
   // matches the filter values
-
+  // 
+  Object.entries(filters).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value);
+  });
 
   // Finally, rebuild the table using the filtered Data
   buildTable(filteredData);
 }
 
+// function to clear table filters by reloading the page
+function clearFilters() {
+  location.reload();
+}
+
 // Attach an event to listen for changes to each filter
 // Hint: You'll need to select the event and what it is listening for within each set of parenthesis
-d3.selectAll().on();
+d3.selectAll("#filter-btn").on("click", updateFilters);
+d3.selectAll("#clear-btn").on("click", clearFilters);
+
 
 // Build the table when the page loads
 buildTable(tableData);
